5bd9ab1d54d17a55b753b8b3a5ceb4f4420f165a
T Steere <thomas.s.steere@gmail.com>
2024-01-12 20:00:29 +1300

    init price response to {}

--- tampermonkey-ai-sbc.user.js
+++ tampermonkey-ai-sbc.user.js
@@ -861,7 +861,7 @@
 			const futBinResponse = await makeGetRequest(
 				`https://www.futbin.com/24/playerPrices?player=${primaryId}&rids=${refIds}`
 			);
-			let priceResponse;
+			let priceResponse = {};
 			try {
 			    priceResponse = JSON.parse(futBinResponse);
 			} catch (error) {
@@ -870,10 +870,11 @@
 				await wait()
 				continue;
 			}
+			
 			for (const id of [primaryId, ...playersIdArray]) {
-				const prices = priceResponse[id].prices[getUserPlatform()];
+				const prices = priceResponse[id]?.prices[getUserPlatform()];
 
-				const lcPrice = prices.LCPrice;
+				const lcPrice = prices?.LCPrice;
 				if (!lcPrice) {
 					continue;
 				}
